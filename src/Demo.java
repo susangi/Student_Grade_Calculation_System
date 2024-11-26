46c34334a21fa795fb3c06e584e5e4df10d8b3f4
Susangi Charuni <susangi@mylinex.com>
2024-11-26 13:28:36 +0530

    create 5 students from using student class

--- src/Demo.java
+++ src/Demo.java
@@ -6,27 +6,27 @@ public class Demo {
         try {
             // used parameterise contractor
             Student s1 = new Student("Susangi", "susangi@gmail.com", 28, "Gampaha", 74);
-            s1.displayStudentDetails();
-            System.out.println();
+//            s1.displayStudentDetails();
+//            System.out.println();
 
             // get 5 student data form user input
             int countOfStudents = 2;
             Student[] students = new Student[countOfStudents];
             for (int i = 0; i < countOfStudents; i++) {
-                System.out.println("Student #" + (i + 1) + " details enter -");
+                System.out.println("Student " + (i + 1) + " details enter -");
                 students[i] = new Student();
                 students[i].obtainDetails();
             }
 
-            // Display details of all students after input is complete
-            System.out.println("\nAll Student Details:");
+            // show all student data
+            System.out.println("All Student Details:");
             for (int i = 0; i < countOfStudents; i++) {
-                System.out.println("\nDetails for Student #" + (i + 1) + ":");
-                students[i].displayStudentDetails(); // Display details for each student
+                System.out.println("Details for Student " + (i + 1) + ":");
+                students[i].displayStudentDetails();
                 System.out.println();
             }
-        } catch (Exception $exception) {
-            System.out.println($exception.getMessage());
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
         }
     }
 }
